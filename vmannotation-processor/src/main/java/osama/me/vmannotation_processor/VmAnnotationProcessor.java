2d11f098a373c68fbf1c05cb126b3c9e0e08ee51
osamarao <osama.rao@willowtreeapps.com>
2018-01-31 14:17:25 -0500

    Added manifest entry for the annotation processor

--- vmannotation-processor/src/main/java/osama/me/vmannotation_processor/VmAnnotationProcessor.java
+++ vmannotation-processor/src/main/java/osama/me/vmannotation_processor/VmAnnotationProcessor.java
@@ -1,5 +1,7 @@
 package osama.me.vmannotation_processor;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -19,13 +21,17 @@ public class VmAnnotationProcessor extends AbstractProcessor {
     }
 
     @Override
-    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnv) {
-//        System.out.println(Arrays.deepToString(set.toArray()));
-//        System.err.println(" OSAMA ");
-        messager.printMessage(Diagnostic.Kind.ERROR, "Osama");
 
+    public boolean process(Set<? extends TypeElement> set, RoundEnvironment roundEnv) {
+        messager.printMessage(Diagnostic.Kind.OTHER, "Osama");
+        messager.printMessage(Diagnostic.Kind.OTHER, Arrays.deepToString(set.toArray()));
         return false;
     }
 
-
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> stringSet = new HashSet<>();
+        stringSet.add("osama.me.vmannotation.BindFields");
+        return stringSet;
+    }
 }
